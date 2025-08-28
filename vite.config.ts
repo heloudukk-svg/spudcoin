a303b01896cf7622bbf9df9441a764c976315862
heloudukk-svg <heloudukk@gmail.com>
2025-08-28 23:11:11 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // If you publish to GitHub Pages under /<repo>/, change base to '/<repo>/'
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/spudcoin/',
 })
